bd6322edc771ccb388592362dc0cf14b4f00f599
yuri753 <55607609+yuri753@users.noreply.github.com>
2026-09-24 13:19:13 -0300

    Delete app directory

--- app/src/commission-preview/review-site.js
+++
@@ -1,4 +0,0 @@
-// COLE O LINK DO SITE PUBLICADO AQUI, entre as aspas.
-// Exemplo: 'https://meu-site.com/' ou 'https://meu-site.com/revisao/'
-// O app acrescenta automaticamente ?key=... para identificar a comissão.
-export const REVIEW_SITE_URL = '';
